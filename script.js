21742de5a01a78dfcb65c2d375284ffe17fcf97c
Mr-Shahadat321 <sahadat8278@gmail.com>
2025-08-27 22:18:50 +0800

    connect card heart icone with nav and count js loop through

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Check to connection');
+const crtBtns = document.getElementsByClassName("crtBtn")
+
+for(const crtBtn of crtBtns){
+    crtBtn.addEventListener('click', function(){
+        const count = parseInt(document.getElementById('navbar-heart-count').innerText);
+        const newCount = count + 1
+        document.getElementById("navbar-heart-count").innerText = newCount
+        
+       
+    })
+}
