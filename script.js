d715a32a1e549d3e1a0d8c841fabf006e21e1b2d
Mr-Shahadat321 <sahadat8278@gmail.com>
2025-08-28 23:51:42 +0800

    set alert, with coin decrement and set time for history list

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//for increment heart icone
+
 const crtBtns = document.getElementsByClassName("crtBtn");
 
 for (const crtBtn of crtBtns) {
+  console.log(crtBtn);
   crtBtn.addEventListener("click", function () {
     const navHeartCount = Number(
       document.getElementById("navbar-heart-count").innerText
@@ -11,6 +14,7 @@ for (const crtBtn of crtBtns) {
   });
 }
 
+//calling functionalities
 function getElement(id) {
   const element = document.getElementById(id);
   return element;
@@ -20,20 +24,53 @@ const callBtns = document.getElementsByClassName("callBtn");
 
 for (const callBtn of callBtns) {
   callBtn.addEventListener("click", function () {
-    const navbarCoin = getElement("navbar-coin").innerText;
+    const navbarCoin = Number(getElement("navbar-coin").innerText);
     const serviceName =
       callBtn.parentNode.parentNode.children[1].children[0].innerText;
     const serviceNumber =
       callBtn.parentNode.parentNode.children[2].children[0].innerText;
-    const historyList = getElement("history-list");
-    const newHistory = document.createElement("div");
-    newHistory.innerHTML = `
-                               <div   class="bg-gray-100 rounded-lg m-4 py-3 pl-2 font-bold">
-                             <h1>${serviceName}</h1>
-                                   <p class="font-bold">${serviceNumber}</p>
-                             </div>
-                   `;
+      
+      if(navbarCoin >= 20){
+        const newCoin = navbarCoin - 20;
+         getElement("navbar-coin").innerText = newCoin
+         alert(`Calling ${serviceName} ${serviceNumber}`)
+         const now = new Date()
+         const localTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-       historyList.append(newHistory)            
+         const historyList = getElement("history-list");
+         const newHistory = document.createElement("div");
+         newHistory.innerHTML = `         
+                              <div class="bg-gray-100 rounded-lg m-4 py-3 pl-2 font-bold flex items-center justify-between">
+                                <div>
+                                        <h1>${serviceName}</h1>
+                                         <p class="font-bold">${serviceNumber}</p>
+
+                                </div>
+                                 <div class="text-sm text-gray-600 pr-3">${localTime}</div>
+                                </div>         
+                   `
+                   historyList.appendChild(newHistory)
+
+      }else{
+        alert(`dont have sufficient coin for calling`)
+      }        
   });
 }
+
+//copy functionalities
+
+const copyBtns = document.getElementsByClassName("copyBtn");
+const navbarCopyCount = document.getElementById("navbar-copy-count")
+
+
+for(const copyBtn of copyBtns){
+  copyBtn.addEventListener('click', function(){
+    
+  })
+}
+
+
+
+
+
+
